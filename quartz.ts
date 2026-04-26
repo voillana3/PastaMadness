bc017286f80a2a639cda9d307d66d788af6ea498
voillana3 <voillana3@gmail.com>
2026-04-25 23:13:55 -0400

    remove read time

--- quartz.ts
+++ quartz.ts
@@ -3,10 +3,3 @@ import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/conf
 const config = await loadQuartzConfig()
 export default config
 export const layout = await loadQuartzLayout()
-
-import * as ExternalPlugin from "./.quartz/plugins";
-
-ExternalPlugin.ContentMeta({
-    showReadingTime: false,
-    showComma: false,
-});
